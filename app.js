55ed14eafe2954a7203ed615bae471a408a56f3e
CatalaHD <thecatalahd@gmail.com>
2020-01-26 10:55:38 +0100

    ara si

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("_config.json");
+const config = require("config.json");
 const ytdl = require("ytdl-core");
 
 let servers = {};
