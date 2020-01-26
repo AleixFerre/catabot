3fdeeef194246cc0a9ae6c3444a23d1c2891fac4
CatalaHD <thecatalahd@gmail.com>
2020-01-26 12:21:06 +0100

    update leave_join working (play idk why not)

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("config.json");
+const config = require("./config.json");
 const ytdl = require("ytdl-core");
 
 let servers = {};
 let prefix = config.prefix;
 
 client.on("ready", () => {
-    console.log("Estic llest!");
+    console.log("Estic llest! Ver: " + config.version);
     client.user.setPresence({
         status: "online",
         game: {
-            name: prefix + "help",
+            name: prefix + "help :: Version " + config.version,
             type: "PLAYING"
         }
     });
@@ -33,7 +33,17 @@ client.on('message', (message) => {
                     m.edit(`:incoming_envelope: Ping Missatges: \`${Math.floor(m.createdTimestamp - Date.now())} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
                 });
         break;
-    
+        
+        case 'join':
+            message.channel.send("Intentant entrar al canal de veu...").then(m => {
+                if (message.member.voiceChannel) {
+                    message.member.voiceChannel.join();
+                    m.edit("He entrat al canal " + message.member.voiceChannel.name);
+                } else {
+                    m.edit("Posa't a un canal de veu perquè pugui unir-me.");
+                }
+            });
+        break;
 
         case 'prefix':
             if (!args[0]) {
@@ -55,10 +65,16 @@ client.on('message', (message) => {
 
         case 'play':
             function play (connection, message) {
-                let server = servers[message.guild.id];
-                servers.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+                var server = servers[message.guild.id];
+                
+                console.log(server.queue[0]);
+
+                servers.dispatcher = connection.playStream(ytdl(server.queue[0].toString(), {filter: "audioonly"}));
+                
                 server.queue.shift();
-                server.dispatcher.on("end", function(){
+                
+                server.dispatcher.on("end", function() {
                     if (server.queue[0]) {
                         play(connection, message);
                     } else {
@@ -72,47 +88,57 @@ client.on('message', (message) => {
                 return;
             }
     
-            if (!message.channel.voiceChannel) {
+            if (!message.member.voiceChannel) {
                 message.channel.send("Posa't a un canal de veu perquè pugui unir-me.");
                 return;
             }
     
             // Creem la cua de cançons...
             if (!servers[message.guild.id]) {
-                servers.guild.id = {
+                servers[message.guild.id] = {
                     queue: []
                 };
             }
     
-            let server = servers[message.guild.id];
-    
-            server.queue.push(args[1]);
+            var server = servers[message.guild.id];
+            
+            server.queue.push(args[0]);
     
             if (!message.guild.voiceConnection) {
                 message.member.voiceChannel.join().then(function(connection) {
                     play(connection, message);
                 });
-                
             }
         break;
 
         case 'leave':
-            let canalvoz = message.member.voiceChannel;
-            if (!canalvoz) {
-                message.channel.send('No estic en cap canal de veu!');
+            if(!message.guild.voiceConnection) {
+                message.channel.send('No estic a cap canal de veu!');
+                return;
+            }
+
+            let userVoiceChannel = message.member.voiceChannel;
+            if (!userVoiceChannel) {
+                message.channel.send('No estàs a cap canal!');
+                return;
+            }
+
+            let clientVoiceConnection = message.guild.voiceConnection;
+            if (userVoiceChannel === clientVoiceConnection.channel) {
+                clientVoiceConnection.disconnect();
+                message.channel.send('Desconnectat correctament.');
             } else {
-                message.channel.send('Deixant el canal de veu...').then(() => {
-                    canalvoz.leave();
-                }).catch(error => message.channel.send(error));
+                message.channel.send('Només pots executar aquesta comanda si estàs al mateix canal que el bot!');
             }
         break;
 
         case 'help':
             message.channel.send('**COMANDES DEL CATABOT**\n```\n' +
-            '-> ' + prefix + 'ping           :: Comprova la latencia del bot i dels teus missatges.\n' +
-            '-> ' + prefix + 'prefix         :: Et mostra el prefix i et permet cambiar-lo amb un segon argument\n' +
-            '-> ' + prefix + 'play           :: Posa la musica que vulguis amb un link\n' +
-            '-> ' + prefix + 'leave          :: Se\'n va del canal de veu.\n```\n\n');
+            '-> ' + prefix + 'ping      :: Comprova la latencia del bot i dels teus missatges.\n' +
+            '-> ' + prefix + 'prefix    :: Et mostra el prefix i et permet cambiar-lo amb un segon argument\n' +
+            '-> ' + prefix + 'join      :: Entra dins del teu canal de veu\n' +
+            '-> ' + prefix + 'play      :: Posa la musica que vulguis amb un link\n' +
+            '-> ' + prefix + 'leave     :: Se\'n va del canal de veu.\n```\n\n');
         break;
 
     }
