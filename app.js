9640e5ac3fba15ca2d40183aba1fbf4ce92925f3
CatalaHD <thecatalahd@gmail.com>
2020-01-26 10:54:20 +0100

    this thing... :/

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require("_config.json");
 const ytdl = require("ytdl-core");
 
 let servers = {};
