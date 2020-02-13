4dab0ba296eec2410f04ac06d6633fe4f6529d59
CatalaHD <thecatalahd@gmail.com>
2020-02-13 12:20:48 +0100

    Refactored !cat

--- commands/cat.js
+++ commands/cat.js
@@ -10,18 +10,26 @@ module.exports = {
 	name: 'cat',
 	description: 'Mostra una imatge d\'un gat aleatori',
 	type: 'entreteniment',
-	execute(message) {
+	async execute(message) {
 
         let $ = require('jquery')(window);
+        let title = "Gatito";
+        let catUrl = "";
         
-        $.getJSON( "https://api.thecatapi.com/v1/images/search?api_key='" + config.catAPIKey + "'?mime_types=gif", function(data) {
-            
-            const exampleEmbed = new Discord.RichEmbed()
-            .setColor('#0099ff')
-            .setTitle('Mira quin gatet més mono! :3')
-            .setImage(data[0].url);
-            
-            message.channel.send(exampleEmbed);
-        });
+        async function getCat() {
+            await $.getJSON( "https://api.thecatapi.com/v1/images/search?api_key='" + config.catAPIKey + "'?mime_types=gif", (data) => {
+                catUrl = data[0].url;
+            });
+        }
+
+        await getCat().catch(console.error);
+    
+        const exampleEmbed = new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle(title)
+        .setImage(catUrl);
+                
+        message.channel.send(exampleEmbed);
+        
 	},
 };
