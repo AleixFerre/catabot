b393eb2737b43c162cc47c714da3347d2663f8fa
CatalaHD <thecatalahd@gmail.com>
2020-02-19 22:03:36 +0100

    added Dice command entreteniment completed

---
+++ commands/dice.js
@@ -0,0 +1,59 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'dice',
+	description: 'Tira els daus que vulguis com vulguis',
+	usage: '[ size ] [ num of dice ]',
+    aliases: ['roll'],
+	type: 'entreteniment',
+	execute(message, args) {
+
+        let size = 6;
+        let num = 1;
+
+        if (args[0]) {
+            if (!isNaN(args[0]))
+                size = args[0];
+            else
+                return message.reply("has de posar un numero!");
+        }
+        if (args[1]) {
+            if (!isNaN(args[1]))
+                num = args[1];
+            else
+                return message.reply("has de posar un numero!");
+        }
+
+        if (size < 0) {
+            return message.reply("la mida del dau ha de ser positiva!");
+        }
+
+        if (num < 1) {
+            return message.reply("el numero de tirades ha de ser major que 1!");
+        } else if (num > 25) {
+            return message.reply("el numero de tirades no pot ser major que 25!");
+        }
+
+        function getRandomColor() {
+            let letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        let msg = new Discord.RichEmbed()
+        .setColor(getRandomColor())
+        .setTitle("**ROLL A DICE**")
+        .setAuthor('CataBOT', 'https://i.imgur.com/UXoPSuU.jpg', 'https://github.com/CatalaHD/DiscordBot')
+        .setThumbnail('http://bit.ly/CataBot_Dice')
+        .setTimestamp().setFooter("Catabot 2020 © All rights reserved");
+
+        for(let i = 0; i<num; i++) {
+            msg.addField("Resultat "+(i+1), Math.round(Math.random()*(size-1))+1, true); // Result 1:size
+        }
+
+        message.channel.send(msg);
+	},
+};
