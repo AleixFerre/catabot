20466b81877cb0a4dc8f09bea8d2eeaf6afb2418
CatalaHD <thecatalahd@gmail.com>
2020-04-26 02:04:07 +0200

    ping updated

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,11 @@
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
-	execute(message) {
+    name: 'ping',
+    description: 'Retorna el ping del servidor i de la API!',
+    type: 'altres',
+    execute(message) {
         var ping = Math.floor(message.client.ping);
         message.channel.send(":ping_pong: Pong!").then((m) => {
             m.edit(`:incoming_envelope: Ping Missatges: \`${Math.floor(m.createdTimestamp - message.createdTimestamp)} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
         }).catch(console.error);
-	},
+    },
 };
