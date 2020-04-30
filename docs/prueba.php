547f7b1ed4b1740451eeaddd030af65359d2bef1
CatalaHD <thecatalahd@gmail.com>
2020-04-30 05:21:15 +0200

    Commands page

---
+++ docs/prueba.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+        <title>Prova</title>
+    </head>
+
+
+<?php
+
+$hola = "Hola";
+print $hola;
+
+?>
---
+++ docs/table.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>CataBot - Comandes</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+
+<body>
+
+<?php
+
+	echo "Hola";
+
+?>
+
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100 ver1">
+					<div class="table100-firstcol">
+						<table>
+							<thead>
+								<tr class="row100 head">
+									<th class="cell100 column1">Employees</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr class="row100 body">
+									<td class="cell100 column1">Brandon Green</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Kathy Daniels</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Elizabeth Alvarado</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Michael Coleman</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Jason Cox</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Christian Perkins</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Emily Wheeler</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					
+					<div class="wrap-table100-nextcols js-pscroll">
+						<div class="table100-nextcols">
+							<table>
+								<thead>
+									<tr class="row100 head">
+										<th class="cell100 column2">Position</th>
+										<th class="cell100 column3">Start date</th>
+										<th class="cell100 column4">Last Activity</th>
+										<th class="cell100 column5">Contacts</th>
+										<th class="cell100 column6">Age</th>
+										<th class="cell100 column7">Address</th>
+										<th class="cell100 column8">Card No</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr class="row100 body">
+										<td class="cell100 column2">CMO</td>
+										<td class="cell100 column3">16 Nov 2012</td>
+										<td class="cell100 column4">16 Nov 2017</td>
+										<td class="cell100 column5">brandon94@example.com</td>
+										<td class="cell100 column6">30</td>
+										<td class="cell100 column7">New York City, NY</td>
+										<td class="cell100 column8">424242xxxxxx6262</td>
+									</tr>
+
+									<tr class="row100 body">
+										<td class="cell100 column2">Marketing</td>
+										<td class="cell100 column3">16 Nov 2015</td>
+										<td class="cell100 column4">30 Nov 2017</td>
+										<td class="cell100 column5">kathy_82@example.com</td>
+										<td class="cell100 column6">26</td>
+										<td class="cell100 column7">New York City, NY</td>
+										<td class="cell100 column8">424242xxxxxx1616</td>
+									</tr>
+
+									<tr class="row100 body">
+										<td class="cell100 column2">CFO</td>
+										<td class="cell100 column3">16 Nov 2013</td>
+										<td class="cell100 column4">30 Nov 2017</td>
+										<td class="cell100 column5">elizabeth82@example.com</td>
+										<td class="cell100 column6">32</td>
+										<td class="cell100 column7">New York City, NY</td>
+										<td class="cell100 column8">424242xxxxxx5326</td>
+									</tr>
+
+									<tr class="row100 body">
+										<td class="cell100 column2">Designer</td>
+										<td class="cell100 column3">16 Nov 2013</td>
+										<td class="cell100 column4">30 Nov 2017</td>
+										<td class="cell100 column5">michael94@example.com</td>
+										<td class="cell100 column6">22</td>
+										<td class="cell100 column7">New York City, NY</td>
+										<td class="cell100 column8">424242xxxxxx6328</td>
+									</tr>
+
+									<tr class="row100 body">
+										<td class="cell100 column2">Developer</td>
+										<td class="cell100 column3">16 Nov 2017</td>
+										<td class="cell100 column4">30 Nov 2017</td>
+										<td class="cell100 column5">jasoncox@example.com</td>
+										<td class="cell100 column6">25</td>
+										<td class="cell100 column7">New York City, NY</td>
+										<td class="cell100 column8">424242xxxxxx7648</td>
+									</tr>
+
+									<tr class="row100 body">
+										<td class="cell100 column2">Sale</td>
+										<td class="cell100 column3">16 Nov 2016</td>
+										<td class="cell100 column4">30 Nov 2017</td>
+										<td class="cell100 column5">christian_83@example.com</td>
+										<td class="cell100 column6">28</td>
+										<td class="cell100 column7">New York City, NY</td>
+										<td class="cell100 column8">424242xxxxxx4152</td>
+									</tr>
+
+									<tr class="row100 body">
+										<td class="cell100 column2">Support</td>
+										<td class="cell100 column3">16 Nov 2013</td>
+										<td class="cell100 column4">30 Nov 2017</td>
+										<td class="cell100 column5">emily90@example.com</td>
+										<td class="cell100 column6">24</td>
+										<td class="cell100 column7">New York City, NY</td>
+										<td class="cell100 column8">424242xxxxxx6668</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+	<script>
+		$('.js-pscroll').each(function(){
+			var ps = new PerfectScrollbar(this);
+
+			$(window).on('resize', function(){
+				ps.update();
+			})
+
+			$(this).on('ps-x-reach-start', function(){
+				$(this).parent().find('.table100-firstcol').removeClass('shadow-table100-firstcol');
+			});
+
+			$(this).on('ps-scroll-x', function(){
+				$(this).parent().find('.table100-firstcol').addClass('shadow-table100-firstcol');
+			});
+
+		});
+
+		
+		
+		
+	</script>
+<!--===============================================================================================-->
+</body>
+</html>
