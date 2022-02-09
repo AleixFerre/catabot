c134d2c41fc73040b6430b5fb2fc22dc7afac73b
Aleix Ferré <thecatalahd@gmail.com>
2022-02-09 12:24:14 +0100

    improving page

--- docs/js/fetchCounter.js
+++ docs/js/fetchCounter.js
@@ -1,7 +1,7 @@
 fetch('./Storage/info.json')
   .then((info) => info.json())
   .then(({ nMembers, nServers, nCommands }) => {
-    document.getElementById('nMembers').innerHTML = nMembers;
+    document.getElementById('nMembers').innerHTML = '+' + Math.floor(nMembers / 1000) * 1000;
     document.getElementById('nServers').innerHTML = nServers;
     document.getElementById('nCommands').innerHTML = nCommands;
   });
