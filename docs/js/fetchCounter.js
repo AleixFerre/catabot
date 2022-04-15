43c9fe3cffc5fe910027b5c1cedeed396bc14bc6
Aleix Ferré <thecatalahd@gmail.com>
2022-04-15 05:13:23 +0200

    removing docs from dev

--- docs/js/fetchCounter.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-fetch('./Storage/info.json')
-	.then((info) => info.json())
-	.then(({ nMembers, nServers, nCommands }) => {
-		document.getElementById('nMembers').innerHTML = '+' + Math.floor(nMembers / 1000) * 1000;
-		document.getElementById('nServers').innerHTML = nServers;
-		document.getElementById('nCommands').innerHTML = nCommands;
-	});
